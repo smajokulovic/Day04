37a97816c100635555fe122e0f6b33bcdd00a4a2
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 20:43:13 +0100

    Multiplication and Division

---
+++ ex09/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+
+    firstNum = 6
+    secondNum = 25
+    thirdNum = 5.2
+
+    // Only change code below this line
+     firstNum *= 9;
+     secondNum /= 5;
+     thirdNum *= 10;
+     // Only change code above this line
+
+
+     return {
+         firstNum,
+         secondNum,
+         thirdNum
+     };
+}
+
+console.log(main());
+module.exports = main;
