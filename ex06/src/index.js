11c985c28d916a6e2e814e102d25c56715495c53
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 19:57:30 +0100

    Divide Decimal

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+
+    myQuotient = numOne / numTwo;
+
+    // Only change code below this line
+     var numOne;
+     var numTwo;
+     // Only change code above this line
+
+
+     return myQuotient;
+}
+
+console.log(main(5.0, 2.0)); // chenge this line
+console.log(main(16.5, 5.5)); // change this line
+console.log(main(102.0, 25.5)); //change this line
+module.exports = main;
