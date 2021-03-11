c705591283b615c12020c3ee35e664fdd34dc2a3
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 21:14:11 +0100

    myHomeTown

---
+++ ex10/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+
+    myFirstName = "Smajo";
+    myLastName = "Kulovic";
+    myHomeTown = "Sarajevo";
+
+    // Only change code below this line
+     
+     // Only change code above this line
+
+
+     return {
+         myFirstName,
+         myLastName,
+         myHomeTown
+     };
+}
+
+console.log(main());
+module.exports = main;
