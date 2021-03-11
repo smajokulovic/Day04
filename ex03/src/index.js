2285be9c291a8bb6021e8b2517f01a0c43dc8bd6
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 18:01:44 +0100

    decrement

---
+++ ex03/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    var myRes = 16;
+    
+    
+    // Only change code below this line 
+
+    // Only change code above this line
+
+    --myRes
+    return myRes;
+}
+console.log(main());
+module.exports = main
