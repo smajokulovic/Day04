dea93891b87370e8fa41e968d03a6c6747155068
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 19:44:28 +0100

    Multiplying two decimals

---
+++ ex05/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwo) {
+
+    product = numOne * numTwo;
+
+    // Only change code below this line
+     var numOne;
+     var numTwo;
+     // Only change code above this line
+
+
+     return product;
+}
+
+console.log(main(2.5, 2.0)); // chenge this line
+console.log(main(5.5, 5.5)); // change this line
+console.log(main(100.25, 43.20)); //change this line
+module.exports = main;
+
+
+
