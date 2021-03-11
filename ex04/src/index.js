df6049c991d5edfa44d392e3e4bcafc7504684da
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 18:41:55 +0100

    Decimal Numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    myDecimal = 8.2
+    myDecimalE = 2.54e-3
+    
+    
+    // Only change code below this line 
+
+    // Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+console.log(main());
+module.exports = main
