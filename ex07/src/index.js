d5d3ecf9eb1f7c3832371bd4fd2d7b841028a86a
Smajo K <smajo.kulovic2512@gmail.com>
2021-03-11 20:15:23 +0100

    remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+
+    remainder = numOne % numTwo;
+
+    // Only change code below this line
+     var numOne;
+     var numTwo;
+     // Only change code above this line
+
+
+     return remainder;
+}
+
+console.log(main(5, 2)); // chenge this line
+console.log(main(15, 4)); // change this line
+console.log(main(102, 25)); //change this line
+module.exports = main;
